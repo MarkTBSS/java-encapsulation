bad40ca017e5a1da1349430fb962cf0a278925bc
MarkTBSS <marktbss@gmail.com>
2024-02-17 00:04:03 +0700

    first commit

---
+++ Game.java
@@ -0,0 +1,20 @@
+public class Game {
+    public static void main(String[] args) {    
+        Player player1 = new Player("John", 100);
+        // Accessing player information using getter methods
+        System.out.println("Player Name: " + player1.getName());
+        System.out.println("Player Health: " + player1.getHealth());
+
+        // Modifying player information using setter methods
+        player1.setName("Jane");
+        player1.setHealth(80);
+        player1.displayInfo();
+
+        // Displaying updated player information
+        player1.deleteHealth(50);
+        player1.displayInfo();
+
+        player1.deleteHealth(40);
+        player1.displayInfo();
+    }
+}
---
+++ Player.java
@@ -0,0 +1,27 @@
+public class Player {
+    private String name;
+    private int health;
+    // Constructor
+    public Player(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+    // Getter and setter methods for name
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    // Getter and setter methods for health
+    public int getHealth() { return health; }
+    public void setHealth(int health) { this.health = health; }
+    public void displayInfo() {
+        System.out.println("Player Name: " + name);
+        System.out.println("Player Health: " + health);
+    }
+    // Method to display player information
+    public void deleteHealth(int damage) {
+        if (damage >= health) {
+            health = 0;
+        } else {
+            health -= damage;
+        }
+    }
+}
